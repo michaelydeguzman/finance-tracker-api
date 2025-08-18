2db378d58db3bd21c3ffb1c46baaa2b05c079554
Michael Enry De Guzman <michaelydeguzman@gmail.com>
2025-08-17 17:08:05 -0700

    initial changes and gitignore

---
+++ Finance.Tracker.Domain/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace FinanceTracker.Domain.Entities
+{
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public required string CategoryName { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ FinanceTracker/Controllers/SettingsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceTracker.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SettingsController : ControllerBase
+    {
+        public SettingsController()
+        {
+
+        }
+
+        [HttpGet(Name = "FetchIncomeCategories")]
+        public IEnumerable<Tas> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
